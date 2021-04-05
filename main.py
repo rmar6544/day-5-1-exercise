d7ff8b3ca5ad421eb71bfcea60c23e88ee54dd7c
Romy M <>
2021-04-05 20:39:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  print(n)
+  # student_heights[n] = int(student_heights[n])
+  print(student_heights)  
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+ 
+
+
+
+
+
